0212ddf2e4a2c6b4b64a84a8c0e61d76fcf289d8
morishitan <0716kouki0716@gmail.com>
2025-12-12 20:50:17 +0900

    add testbag

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,5 +4,5 @@ def test_health():
     app.config["TESTING"] = True
     with app.test_client() as c:
         r = c.get("/health")
-        assert r.status_code == 200
+        assert r.status_code == 404
         assert r.get_json()["status"] == "ok"
